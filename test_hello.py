4cc2ea8f48e7bcf96a121d7d1adce6c7e9788b5e
Oreo_the_cat <162893603+UmeshDhanokar@users.noreply.github.com>
2026-04-23 15:46:25 +0200

    Delete test_hello.py

--- test_hello.py
+++
@@ -1,8 +0,0 @@
-# test_hello.py
-def test_hello_world():
-    assert "Hello" == "Hello"
-
-def test_main_runs():
-    # Simulates your main.py
-    result = "Hello from GitHub Actions!"
-    assert "Hello" in result
